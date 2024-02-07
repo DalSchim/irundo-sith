e0a6ae05568af83aec454efcb82713096ccb7033
Guillaume Gonidou <31546068+DalSchim@users.noreply.github.com>
2024-02-07 12:04:40 +0100

    fixed error css

--- src/views/FonctionView.vue
+++ src/views/FonctionView.vue
@@ -17,15 +17,24 @@ export default {
     <div class="contaner">
       <h2>Les fonctionnalités</h2>
       <div class="wrapper">
-        <card-component title="Controle domaine" image="mdi:number-1-circle" description="Contrôleur de domaine AD, alimenté depuis les différentes bases disponibles (Pronote/EDT, Siecle/STS ou les ENT)"/>
-        <card-component title="Serveur de fichier" image="mdi:number-2-circle" description="Serveur de fichiers SMB, NFS4 avec séparation des réseaux "/>
-        <card-component title="Reservation dhcp et ip" image="mdi:number-3-circle" description="DHCP et réservations IP Multi-réseaux et multi-VLANs"/>
-        <card-component title="Gestion parcs" image="mdi:number-4-circle" description="Gestion des parcs, des salles Extinction automatique, démarrage automatique, réinstallation, déploiements"/>
-        <card-component title="Serveur impression" image="mdi:number-5-circle" description="Serveur d'impression SMB/CUPS. Les imprimantes sont déployées de façon native avec des stratégies de groupe (GPO)"/>
-        <card-component title="Déploiment automatique d'application" image="mdi:number-6-circle" description="Déploiement automatique d’applications. Plus de 200 applications sont disponibles et maintenues à jour directement dans SambaÉdu"/>
-        <card-component title="Déploiment automatique d'os" image="mdi:number-7-circle" description="Déploiement automatique de Windows 10, 11 et GNU/Linux Debian avec intégration automatisée au domaine des postes"/>
-        <card-component title="Configuration des profil et des station" image="mdi:number-8-circle" description="Configuration simplifiée des profils et des stations par GPO via interface Web. Aucun client ou agent n’est à déployer sur les postes"/>
-        <card-component title="Configuration des drois et permition" image="mdi:number-9-circle" description="Configuration des rôles et droits sur les différents parcs de machines pour des utilisateurs / groupes d'utilisateurs"/>
+        <card-component title="Controle domaine" image="mdi:number-1-circle"
+                        description="Contrôleur de domaine AD, alimenté depuis les différentes bases disponibles (Pronote/EDT, Siecle/STS ou les ENT)"/>
+        <card-component title="Serveur de fichier" image="mdi:number-2-circle"
+                        description="Serveur de fichiers SMB, NFS4 avec séparation des réseaux "/>
+        <card-component title="Reservation dhcp et ip" image="mdi:number-3-circle"
+                        description="DHCP et réservations IP Multi-réseaux et multi-VLANs"/>
+        <card-component title="Gestion parcs" image="mdi:number-4-circle"
+                        description="Gestion des parcs, des salles Extinction automatique, démarrage automatique, réinstallation, déploiements"/>
+        <card-component title="Serveur impression" image="mdi:number-5-circle"
+                        description="Serveur d'impression SMB/CUPS. Les imprimantes sont déployées de façon native avec des stratégies de groupe (GPO)"/>
+        <card-component title="Déploiment automatique d'application" image="mdi:number-6-circle"
+                        description="Déploiement automatique d’applications. Plus de 200 applications sont disponibles et maintenues à jour directement dans SambaÉdu"/>
+        <card-component title="Déploiment automatique d'os" image="mdi:number-7-circle"
+                        description="Déploiement automatique de Windows 10, 11 et GNU/Linux Debian avec intégration automatisée au domaine des postes"/>
+        <card-component title="Configuration des profil et des station" image="mdi:number-8-circle"
+                        description="Configuration simplifiée des profils et des stations par GPO via interface Web. Aucun client ou agent n’est à déployer sur les postes"/>
+        <card-component title="Configuration des drois et permition" image="mdi:number-9-circle"
+                        description="Configuration des rôles et droits sur les différents parcs de machines pour des utilisateurs / groupes d'utilisateurs"/>
       </div>
     </div>
   </section>
@@ -74,8 +83,17 @@ export default {
       gap: 20px;
     }
 
+  }
+}
 
 
+@media (max-width: 920px) {
+  .fonctionalité-sec {
+    .contaner {
+      .wrapper {
+        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+      }
+    }
   }
 }
 
